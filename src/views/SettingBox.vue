44dc9ef498a665998db41e3cea2cb5aa4e0ab9ff
王凤祥 <1205679862@qq.com>
2024-07-04 10:32:31 +0800

    feat: 文案

--- src/views/SettingBox.vue
+++ src/views/SettingBox.vue
@@ -2,7 +2,7 @@
  * @Description: 全局设置面板
  * @Author: wangfengxiang
  * @Date: 2024-07-03 16:17:06
- * @LastEditTime: 2024-07-03 17:01:11
+ * @LastEditTime: 2024-07-04 10:26:31
  * @LastEditors: wangfengxiang
 -->
 <template>
@@ -13,6 +13,9 @@
             模版中
             <span>{top}</span> 和
             <span>{left}</span> 将被替换为元素位移的top和left值，并自动复制到剪切板中
+        </div>
+        <div class="setting-subtitle">
+            默认：<span>position: absolute; top: {top}px; left: {left}px;</span>
         </div>
         <input
             type="text"
@@ -43,6 +46,9 @@ const { draftsInfo } = useDrafts()
         line-height: 16px;
         margin: 5px 0;
         color: #ccc;
+        span {
+            color: #7cacf8;
+        }
     }
     .code-input {
         .size(100%, 30px);
